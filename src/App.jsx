529bf027c0dbbf24492db489fe37ea070ecea685
Siddhartha51 <thapasiddharth51@gmail.com>
2026-08-14 22:58:13 +0545

    Project setup, home page, navigations

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
+import Skills from "./pages/Skills";
+import Projects from "./pages/Projects";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/skills" element={<Skills />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  const navLinks = [
+    { name: "Home", path: "/" },
+    { name: "About", path: "/about" },
+    { name: "Skills", path: "/skills" },
+    { name: "Projects", path: "/projects" },
+    { name: "Contact", path: "/contact" },
+  ];
+
+  return (
+    <>
+      <header className="w-full bg-black text-white">
+        <div className="max-w-7xl mx-auto py-4 flex items-center justify-between">
+          <Link className="font-bold hover:text-purple-600" to="/">Siddhartha</Link>
+        
+        <nav className="flex space-x-8">
+          {navLinks.map((links) => (
+            <Link className="hover:text-purple-600" to={links.path} key={links.name}>
+              {links.name}
+            </Link>
+          ))}
+        </nav>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Navbar;
