e53ff413c68fad02bcbcee5d3db8b65a99050a1f
Siddhartha51 <thapasiddharth51@gmail.com>
2026-08-17 14:37:37 +0545

    project page and contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,64 @@
+import { FaEnvelope, FaMapMarkerAlt, FaBriefcase, FaRocket } from "react-icons/fa";
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div className="min-h-screen bg-gray-950 text-white py-20 px-6">
+      <div className="mx-auto max-w-6xl">
+        <h1 className="text-center font-bold text-6xl mb-5">
+          GetIn <span className="text-purple-500">Touch</span>
+        </h1>
+        <div className="flex flex-row justify-center">
+          <p className="text-center text-xl mb-10 text-gray-400">
+          Have a project or idea? Let's build something amazing
+        </p>
+        <FaRocket className="text-purple-500 shrink-0"/>
+        </div>
+        
+
+        <div className="flex flex-col md:flex-row justify-center items-start gap-12">
+          <div className="max-w-md space-y-4">
+            <h2 className="text-2xl font-bold mb-4">
+              Contact <span className="text-purple-500">Info</span>
+            </h2>
+            <p className="mb-4 text-gray-400">
+              I'm available for freelance work and collaborations.
+            </p>
+            <div className="space-y-3 pt-2 text-gray-300">
+              <p className="flex items-center space-x-3">
+                <FaEnvelope className="text-purple-500 shrink-0" />
+                <span>Email: thapasiddharth51@gmail.com</span>
+              </p>
+              <p className="flex items-center space-x-3">
+                <FaMapMarkerAlt className="text-purple-500 shrink-0" />
+                <span>Location: Bhaktapur, Nepal</span>
+              </p>
+              <p className="flex items-center space-x-3">
+                <FaBriefcase className="text-purple-500 shrink-0" />
+                <span>FullTime/Freelance / Remote Work Available</span>
+              </p>
+            </div>
+          </div>
+
+          <form className="flex flex-col bg-gray-900 w-full max-w-lg border border-gray-800 hover:border-purple-700 space-y-4 rounded-2xl p-8">
+            <input
+              type="text"
+              placeholder="Your Name"
+              className="bg-gray-800 px-4 py-3.5 rounded-xl border border-gray-700/60 focus:border-purple-500 transition-colors"
+            />
+            <input type="email" placeholder="Your Email" 
+            className="bg-gray-800 px-4 py-3.5 rounded-xl border border-gray-700/60 focus:border-purple-500 transition-colors"
+            />
+            <textarea 
+            type="field" 
+            placeholder="Your Message" 
+            className="bg-gray-800 px-4 py-3.5 rounded-xl border border-gray-700/60 focus:border-purple-500 transition-colors"
+            />
+            <button type="submit" className="bg-purple-700 hover:bg-purple-800 rounded-xl p-3 cursor-pointer">Send Message</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,67 @@
+import profile from "../assets/myimg.JPG";
+import portfolio from '../assets/portfolio.png';
 
 function Projects() {
+  const projectDetails = [
+    {
+      image: profile,
+      name: "Portfolio Website",
+      details:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit Repellendus, tenetur, nihil provident ad repellat cupiditate quoddebitis sint laudantium molestiae officia voluptatum suscipit ipsam deserunt sit laboriosam quam. Quaerat, accusantium",
+      techs: ["html", "CSS"],
+      demo: "",
+      code: "",
+    },
+    {
+      image: portfolio,
+      name: "Portfolio Website",
+      details:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit Repellendus, tenetur, nihil provident ad repellat cupiditate quoddebitis sint laudantium molestiae officia voluptatum suscipit ipsam deserunt sit laboriosam quam. Quaerat, accusantium",
+      techs: ["html", "CSS", 'java', 'ggg'],
+      demo: "",
+      code: "",
+    },
+  ];
   return (
-    <div>Projects</div>
-  )
+    <div className="min-h-screen bg-gray-900 text-white py-20 px-6">
+      <div className="max-w-6xl mx-auto">
+        <h1 className="text-center font-bold text-6xl">
+          My <span className="text-purple-600">Projects</span>
+        </h1>
+        <p className="text-center mt-4 text-xl">Projects I've built</p>
+
+        <div className="grid grid-cols-3 pt-5 gap-6">
+          {projectDetails.map((projects, index) => (
+            <div
+              key={index}
+              className="bg-gray-800 rounded-2xl pb-3 border border-gray-800 hover:border-purple-500/50"
+            >
+              <div className="h-70 w-full flex items-center justify-center p-2">
+              <img src={projects.image} alt="" className="h-full rounded" />
+              </div>
+              <h2 className="font-bold text-xl pl-3 pt-3">{projects.name}</h2>
+              <p className="text-gray-400 pl-3 pr-3">{projects.details}</p>
+
+              <div className="flex flex-wrap gap-2 pl-3">
+                {projects.techs.map((tech, index) => (
+                  <span
+                    key={index}
+                    className="bg-gray-500 rounded-full px-3 py-1 mt-3"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex flex-wrap justify-between p-3">
+                <span className="bg-gray-700 rounded-full px-3 py-1">Demo</span>
+                <span className="bg-gray-700 rounded-full px-3 py-1">Code</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
